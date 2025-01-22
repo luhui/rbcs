3167719e931266d2208968ebb86169a19de9e2aa
陆晖 <luhui@mindlinker.com>
2025-01-22 10:13:52 +0800

    [optimization]优化部署脚本

--- build-images.sh
+++ build-images.sh
@@ -1,7 +1,8 @@
 #!/bin/bash
 
 # 构建Java项目
-./gradlew clean build
+./gradlew clean bootJar
 
 # 构建rbcs镜像
-docker build -t rbcs:latest .
+docker build -t 255170271587.dkr.ecr.ap-northeast-1.amazonaws.com/luhui/rbcs:latest .
+docker push 255170271587.dkr.ecr.ap-northeast-1.amazonaws.com/luhui/rbcs:latest
--- deploy.sh
+++ deploy.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 # 检查是否已构建镜像
-if [[ "$(docker images -q rbcs:latest 2> /dev/null)" == "" ]]; then
+if [[ "$(docker images -q 255170271587.dkr.ecr.ap-northeast-1.amazonaws.com/luhui/rbcs:latest 2> /dev/null)" == "" ]]; then
   echo "请先运行build-images.sh构建镜像"
   exit 1
 fi
